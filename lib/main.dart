34c2e147b7918f12b1170a94dae0de62d03e065f
Yona <3211998+yonacwy@users.noreply.github.com.>
2025-01-31 16:40:30 -0500

    Re-added cupertino_icons

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Christian Home Page'),
+      home: const MyHomePage(title: 'Christian App'),
     );
   }
 }
